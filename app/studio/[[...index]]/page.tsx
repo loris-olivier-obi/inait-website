793ab9d7eb7061b02ec59550854d3086e2cad066
loris-olivier-obi <olivier.loris@openbraininstitute.org>
2025-10-30 16:09:39 +0100

    Removed the studio folder

--- app/studio/[[...index]]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../sanity.config";
-
-export default function StudioPage() {
-  // Supports the same config as `Sanity.createClient` for Client Components
-  return <NextStudio config={config} />;
-}
